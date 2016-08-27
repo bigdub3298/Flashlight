ebd1320e359d8645028dc8e65608b518a49693cc
bigdub3298 <worldwidewes@Wesleys-MacBook-Pro.local>
2016-08-27 12:12:59 -0600

    Final Flashlight App
    Flashlight app that can toggle the light on your device by swiping
    right to turn it on and left to turn it off.

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -7,19 +7,100 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var toggleSwipeView: UIView! // View that conatians the UISwipeGestureRecognizer
+    @IBOutlet weak var onOffLabel: UILabel!
+    
+    var device: AVCaptureDevice? = nil
 
+   
+    var isOn: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func toggleSwipeRecognizer(sender: UISwipeGestureRecognizer) {
+        if sender.state == .Ended {
+            
+            //Obtains the media device that controls video
+            let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+            
+            // checks to see if the device has a flash
+            if(device.hasTorch) {
+                do  {
+                    try device.lockForConfiguration()
+                    if isOn {
+                        // toggle flash
+                        device.torchMode = AVCaptureTorchMode.Off
+                        
+                        // Change the direction of the swipe
+                        sender.direction = .Right
+                        
+                        // Change view colors
+                        toggleSwipeView.backgroundColor = .blackColor()
+                        self.view.backgroundColor = .blackColor()
+                        onOffLabel.textColor = .whiteColor()
+                        
+                        // Change text
+                        onOffLabel.text = "On"
+                        
+                        // Change boolean marker
+                        isOn = false
+                    } else {
+                        
+                        do {
+                            try device.setTorchModeOnWithLevel(1.0)
+                        } catch {
+                            print("\(error)")
+                        }
+                        // toggle flash
+                        device.torchMode = AVCaptureTorchMode.On
+                        
+                        // Change the direction of the swipe
+                        sender.direction = .Left
+                        
+                        // Change view colors
+                        toggleSwipeView.backgroundColor = .whiteColor()
+                        self.view.backgroundColor = .whiteColor()
+                        onOffLabel.textColor = .blackColor()
+                        
+                        // Change text
+                        onOffLabel.text = "Off"
+                        
+                        // Change boolean marker
+                        isOn = true
+                    }
+                    toggleStatusBarStyle()
+                } catch {
+                    print("\(error)")
+                }
+                
+          
+            }
+        }
+    }
+    
 
-
+    func toggleStatusBarStyle() {
+        if isOn {
+            UIApplication.sharedApplication().statusBarStyle = .Default
+        } else {
+            UIApplication.sharedApplication().statusBarStyle = .LightContent
+        }
+    }
+        
 }
+
+
 
